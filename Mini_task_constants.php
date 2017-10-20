d3e8e3cbe7d67cb394641e0398bdf5ca2edf7af1
Butler-J-BPC-Cyan <32195079+Butler-J-BPC-Cyan@users.noreply.github.com>
2017-10-20 09:54:59 +0100

    Add files via upload

---
+++ Mini_task_constants.php
@@ -0,0 +1,24 @@
+<HTML>
+<HEAD>
+<TITLE>
+Mini_task
+</TITLE>
+</HEAD>
+
+<BODY>
+<h1>
+Jack Butler, favourite Subject 2D animation.
+</h1>
+<?php
+$myfirst = "hello world ofcourse!";
+$firstNum = 6;
+$firstFloat = 12.5;
+.
+echo $myfirst;
+echo "<br>";
+echo $firstNum;
+echo "<br>";
+echo $firstFloat;
+?>
+</BODY>
+</HTML>
